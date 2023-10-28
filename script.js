4a7f5d45c48af38764193d083a674a80e61d3b12
WaveRaiderCeru <iheb.gabsi@istic.ucar.tn>
2023-10-28 17:11:53 +0100

    update2

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var topset=document.getElementById("topset");
-var image1=document.getElementById("icon01");
+//main
 var hello    = document.getElementById("option01");
 var exhtml   = document.getElementById("option02");
 var excss    = document.getElementById("option03");
@@ -34,4 +33,53 @@ excss.addEventListener("click",show3);
 exjs.addEventListener("click",show4);
 form.addEventListener("click",show5);
 
+///////////////////js page
+var js0 = document.getElementById("jsdef");
+var js1 = document.getElementById("jscalc");
+var js2 = document.getElementById("jsalert");
+var js3 = document.getElementById("jsmulti");
+var js4 = document.getElementById("jssett");
+var js5 = document.getElementById("jshtml");
+var js6 = document.getElementById("jstxt");
+var js7 = document.getElementById("jsevent");
+var js8 = document.getElementById("jssrc");
 
+var sjs1 = document.getElementById("jss1");
+var sjs2 = document.getElementById("jss2");
+var sjs3 = document.getElementById("jss3");
+var sjs4 = document.getElementById("jss4");
+var sjs5 = document.getElementById("jss5");
+var sjs6 = document.getElementById("jss6");
+var sjs7 = document.getElementById("jss7");
+var sjs8 = document.getElementById("jss8");
+
+var alljs = document.querySelectorAll(".js");
+for(var i=0,c=alljs.length;i < c ; i++ ){
+    alljs[i].setAttribute("hidden","1")
+}
+//js0.innerHTML=js1.innerHTML;
+var jsshow1 = function(){js0.innerHTML=js1.innerHTML;}
+var jsshow2 = function(){js0.innerHTML=js2.innerHTML;}
+var jsshow3 = function(){js0.innerHTML=js3.innerHTML;}
+var jsshow4 = function(){js0.innerHTML=js4.innerHTML;}
+var jsshow5 = function(){js0.innerHTML=js5.innerHTML;}
+var jsshow6 = function(){js0.innerHTML=js6.innerHTML;}
+var jsshow7 = function(){js0.innerHTML=js7.innerHTML;}
+var jsshow8 = function(){js0.innerHTML=js8.innerHTML;}
+jsshow2();
+sjs1.addEventListener("click",jsshow1);
+sjs2.addEventListener("click",jsshow2);
+sjs3.addEventListener("click",jsshow3);
+sjs4.addEventListener("click",jsshow4);
+sjs5.addEventListener("click",jsshow5);
+sjs6.addEventListener("click",jsshow6);
+sjs7.addEventListener("click",jsshow7);
+sjs8.addEventListener("click",jsshow8);
+
+
+
+var accb = function{
+    alert("welp");
+}
+var but1=document.getElementById("Abutton");
+but1.addEventListener("click",accb);
